4cafd5a1e9c0521d1b5572c5db2a023fae554993
alokup60 <alokrajsngh@gmail.com>
2023-12-21 15:33:36 +0530

    createEventDispatcher how to use

--- src/routes/cms/+page.svelte
+++ src/routes/cms/+page.svelte
@@ -1,6 +1,5 @@
 <script>
   import imgUploader from "$lib/images/imgUploader.svg";
-  import { ModuleGraph } from "vite";
   import EditModal from "../components/EditModal.svelte";
   export let data;
   export let form;
@@ -69,10 +68,10 @@
     }
     console.log(selectedTags);
   }
-  // function openModal() {
-  //   modal.classList.remove("hidden");
-  //   overlay.classList.remove("hidden");
-  // }
+
+  const handleClose = () => {
+    showModal = false;
+  };
 </script>
 
 <svelte:head>
@@ -85,27 +84,7 @@
   />
 </svelte:head>
 
-<section class="modal hidden">
-  <h2>Edit Tag</h2>
-  <div class="flex justify-between mx-auto items-center">
-    <label for="tag">Tag</label>
-    <input type="text" value="" name="tag" />
-  </div>
-  <div class="flex justify-center items-center gap-4 mx-auto">
-    <button
-      type="button"
-      class="bg-green-400 text-white rounded-md px-2 py-1 hover:bg-green-500 transition-all delay-200"
-      >Update</button
-    >
-    <button
-      type="button"
-      class="bg-red-400 text-white rounded-md px-2 py-1 hover:bg-red-500 transition-all delay-200"
-      >Close</button
-    >
-  </div>
-</section>
-
-<div class="overlay hidden"></div>
+<EditModal show={showModal} on:close={handleClose} />
 
 <div class="w-11/12 flex flex-wrap justify-between mx-auto bg-white mt-[2rem]">
   <form
@@ -265,7 +244,6 @@
 <style>
   .bx {
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-    border-radius: 10px;
   }
   #tags {
     appearance: none;
@@ -276,19 +254,4 @@
   /* .selected-button:hover {
     display: none;
   } */
-  .overlay {
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.5);
-    backdrop-filter: blur(3px);
-    z-index: 1;
-  }
-  .modal {
-    z-index: 2;
-  }
 </style>
--- src/routes/components/EditModal.svelte
+++ src/routes/components/EditModal.svelte
@@ -1,28 +1,36 @@
 <script>
   //   let modal = true;
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  export let show = true;
+  const close = () => {
+    dispatch("close");
+  };
 </script>
 
-<section class="modal hidden">
-  <h2>Edit Tag</h2>
-  <div class="flex justify-between mx-auto items-center">
-    <label for="tag">Tag</label>
-    <input type="text" value="" name="tag" />
-  </div>
-  <div class="flex justify-center items-center gap-4 mx-auto">
-    <button
-      type="button"
-      class="bg-green-400 text-white rounded-md px-2 py-1 hover:bg-green-500 transition-all delay-200"
-      >Update</button
-    >
-    <button
-      type="button"
-      class="bg-red-400 text-white rounded-md px-2 py-1 hover:bg-red-500 transition-all delay-200"
-      >Close</button
-    >
-  </div>
-</section>
-
-<div class="overlay hidden"></div>
+{#if show}
+  <section class="modal">
+    <div class="overlay" on:click={close}></div>
+    <h2>Edit Tag</h2>
+    <div class="flex justify-between mx-auto items-center">
+      <label for="tag">Tag</label>
+      <input type="text" value="" name="tag" />
+    </div>
+    <div class="flex justify-center items-center gap-4 mx-auto">
+      <button
+        type="button"
+        class="bg-green-400 text-white rounded-md px-2 py-1 hover:bg-green-500 transition-all delay-200"
+        >Update</button
+      >
+      <button
+        type="button"
+        on:click={close}
+        class="bg-red-400 text-white rounded-md px-2 py-1 hover:bg-red-500 transition-all delay-200"
+        >Close</button
+      >
+    </div>
+  </section>
+{/if}
 
 <style>
   .overlay {
@@ -38,6 +46,7 @@
     z-index: 1;
   }
   .modal {
+    /* position: absolute; */
     z-index: 2;
   }
 </style>
